33b366f4d2c62adbf314bd07ad5f8f7f6795a0b6
SnowXib <leo.sav00@gmail.com>
2024-11-07 10:32:43 +0300

    fix: случайное изменение файла main.go

--- main.go
+++ main.go
@@ -1,38 +1,12 @@
 package main
 
 import (
-	"LeetCode/middleware"
+	"fmt"
 
-	"github.com/gin-gonic/gin"
+	"LeetCode/medium"
 )
 
 func main() {
-	router := gin.New()
-
-	router.Use(middleware.Authenticate)
-
-	router.GET("/getData", getData)
-	router.GET("/getData1", getData1)
-	router.GET("/getData2", getData2)
-	// router.GET("/getUrlData/:name/:age", getUrlData)
-
-	router.Run()
-}
-
-func getData(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"data": "GET Hiiiiiiiiii",
-	})
-}
-
-func getData1(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"data": "GET1 Hiiiiiiiiii",
-	})
-}
-
-func getData2(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"data": "GET2 Hiiiiiiiiii",
-	})
+	result := medium.LongestPalindrome("xaabacxcabaaxcabaax")
+	fmt.Println("Longest palindrome:", result)
 }
