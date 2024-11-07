e911b71b67e328f0256cf987c80bbd3b8c7d830b
SnowXib <leo.sav00@gmail.com>
2024-11-07 10:31:10 +0300

    feat: Гитигнор

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
-	"fmt"
+	"LeetCode/middleware"
 
-	"LeetCode/medium"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	result := medium.LongestPalindrome("xaabacxcabaaxcabaax")
-	fmt.Println("Longest palindrome:", result)
+	router := gin.New()
+
+	router.Use(middleware.Authenticate)
+
+	router.GET("/getData", getData)
+	router.GET("/getData1", getData1)
+	router.GET("/getData2", getData2)
+	// router.GET("/getUrlData/:name/:age", getUrlData)
+
+	router.Run()
+}
+
+func getData(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"data": "GET Hiiiiiiiiii",
+	})
+}
+
+func getData1(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"data": "GET1 Hiiiiiiiiii",
+	})
+}
+
+func getData2(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"data": "GET2 Hiiiiiiiiii",
+	})
 }
